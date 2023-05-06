b9ef72b8052a974bd5e208e53754a2070580a6a9
hoangvhth2211016 <hoangvhth2211016@fpt.edu.vn>
2023-05-06 17:40:24 +0700

    several change on productsList to meet the requirement of model

--- src/app/demoDatas/productsList.ts
+++ src/app/demoDatas/productsList.ts
@@ -5,7 +5,9 @@ export const productsList = [
     name: 'diamond ring',
     image: 'assets/images/products/diamond_ring.jpg',
     price: 1000,
-    categories: ['diamond', 'ring', 'new arrival'],
+    categories: [
+      { name: 'diamond' }, { name: 'ring' }, { name: 'new arrival' }
+    ],
     description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore facilis sapiente commodi quos minima natus mollitia molestiae nam, animi quae ullam illo incidunt corrupti eveniet amet laboriosam libero, neque temporibus.'
   },
   {
@@ -13,7 +15,9 @@ export const productsList = [
     name: 'amber necklace',
     image: 'assets/images/products/amber_necklace.jpg',
     price: 150,
-    categories: ['amber', 'necklace', 'sale'],
+    categories: [
+      { name: 'amber' }, { name: 'necklace' }, { name: 'sale' }
+    ],
     description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore facilis sapiente commodi quos minima natus mollitia molestiae nam, animi quae ullam illo incidunt corrupti eveniet amet laboriosam libero, neque temporibus.'
   },
   {
@@ -21,7 +25,9 @@ export const productsList = [
     name: 'amethyst ring',
     image: 'assets/images/products/amethyst_ring.jpg',
     price: 200,
-    categories: ['amethyst', 'ring', 'sale'],
+    categories: [
+      { name: 'amethyst' }, { name: 'ring' }, { name: 'sale' }
+    ],
     description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore facilis sapiente commodi quos minima natus mollitia molestiae nam, animi quae ullam illo incidunt corrupti eveniet amet laboriosam libero, neque temporibus.'
   },
   {
@@ -29,7 +35,9 @@ export const productsList = [
     name: 'emerald ring',
     image: 'assets/images/products/emerald_ring.jpg',
     price: 600,
-    categories: ['emerald', 'ring', 'new arrival'],
+    categories: [
+      { name: 'emerald' }, { name: 'ring' }, { name: 'new arrival' }
+    ],
     description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore facilis sapiente commodi quos minima natus mollitia molestiae nam, animi quae ullam illo incidunt corrupti eveniet amet laboriosam libero, neque temporibus.'
   },
   {
@@ -37,7 +45,9 @@ export const productsList = [
     name: 'ruby earring',
     image: 'assets/images/products/ruby_earring.jpg',
     price: 700,
-    categories: ['ruby', 'earring', 'new arrival'],
+    categories: [
+      { name: 'ruby' }, { name: 'earring' }, { name: 'new arrival' }
+    ],
     description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore facilis sapiente commodi quos minima natus mollitia molestiae nam, animi quae ullam illo incidunt corrupti eveniet amet laboriosam libero, neque temporibus.'
   }
 ]
