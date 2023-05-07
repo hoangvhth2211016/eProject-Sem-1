e6832482f99a99516b64f30c4b72651b1a8aaed6
HungDinhTuan <hungtuandinhbg@gmail.com>
2023-05-07 15:18:27 +0700

    add file banner in file components and ad file image banner in file assets

---
+++ src/app/components/banner/banner.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.css']
+})
+export class BannerComponent {
+
+  url1 = ".assets/images/banner/banner-20230307T065108Z-001_1200x450.jpg";
+
+  url2 = "./assets/images/banner/kvv2_1200x450-best_seller.jpg";
+
+}
